11218bbbb2260f0429c8f35f102d098e1b80fa77
Taeyeon <mindc1891@gmail.com>
2021-12-14 21:25:02 +0900

    add:likeview

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import OrderView, CommentView
+from .views import OrderView, CommentView, LikeView
 
 urlpatterns = [
     path('/order/<int:course_id>', OrderView.as_view()),
-    path('/<int:course_id>/comments', CommentView.as_view())
+    path('/<int:course_id>/comments', CommentView.as_view()),
+    path('/like', LikeView.as_view())
 ]
